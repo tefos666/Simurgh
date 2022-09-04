46d9fb06a644963fca7927de8c7b78b5de56f79b
Mohsen Maghsoudi <Mohsen.maghsoudi@gmail.com>
2022-09-04 12:45:13 +0430

    User-Template Configed

---
+++ EndPoint.WEB/Areas/User-Side/Controllers/DashboardController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EndPoint.WEB.Areas.User_Side.Controllers
+{
+    public class DashboardController : Controller
+    {
+        [Area("User-Side")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
