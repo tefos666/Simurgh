89ab51c7ad7d0810e23364ff31ac1e29e5bfde0c
Mohsen Maghsoudi <Mohsen.maghsoudi@gmail.com>
2022-09-04 12:51:48 +0430

    Admin-Side Template Configed

---
+++ EndPoint.WEB/Areas/Admin-Side/Controllers/DashboardController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EndPoint.WEB.Areas.Admin_Side.Controllers
+{
+    public class DashboardController : Controller
+    {
+        [Area("Admin-Side")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
